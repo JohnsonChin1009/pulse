e3de3a1af69ecca0c62a8ed922bf286f97513781
Johnson Chin <johnsonchin1009@gmail.com>
2025-08-12 20:26:47 +0800

    feat: added profile page and image remote patterns

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,23 +1,34 @@
 "use client";
 
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { GoGear } from "react-icons/go";
-import { IoIosArrowRoundForward } from "react-icons/io";
-import { IoInformationCircleOutline } from "react-icons/io5";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
 
 export default function ProfilePage() {
-  const links = [
+  const achievements = [
     {
       id: 1,
-      name: "About Me",
-      target: "/about-me",
-      icon: <IoInformationCircleOutline />,
+      title: "First Words",
+      badgeURL:
+        "https://github.githubassets.com/assets/yolo-default-be0bbff04951.png",
     },
     {
       id: 2,
-      name: "Settings",
-      target: "/settings",
-      icon: <GoGear />,
+      title: "Wave Rider",
+      badgeURL:
+        "https://github.githubassets.com/assets/pull-shark-default-498c279a747d.png",
+    },
+    {
+      id: 3,
+      title: "Mood Streaker",
+      badgeURL:
+        "https://github.githubassets.com/assets/quickdraw-default--light-medium-5450fadcbe37.png",
+    },
+    {
+      id: 4,
+      title: "Vibe Checker",
+      badgeURL:
+        "https://github.githubassets.com/assets/starstruck-default--medium-2670f78c9f2f.png",
     },
   ];
 
@@ -37,29 +48,62 @@ export default function ProfilePage() {
               <AvatarImage src="https://avatars.githubusercontent.com/u/107231772?v=4" />
               <AvatarFallback>JC</AvatarFallback>
             </Avatar>
-            <p className="font-headline">johnson</p>
+            <div className="space-y-1 text-center">
+              <p className="font-headline">johnson</p>
+              <p className="font-main">Journeyman</p>
+            </div>
           </div>
 
-          {/* Menu Buttons */}
-          <nav className="flex flex-col w-full space-y-7">
-            {links.map((item) => (
-              <a
-                href={item.target}
-                key={item.id}
-                className="flex items-center justify-between text-gray-600 hover:text-black"
-              >
-                <div className="flex items-center justify-center space-x-4">
-                  <div className="bg-primary rounded-full border-black border text-xl p-3">
-                    {item.icon}
-                  </div>
-                  <p>{item.name}</p>
+          {/* Statistics Section */}
+          <div className="flex flex-col space-y-8">
+            <div className="flex items-center gap-3">
+              <Separator className="flex-1 bg-foreground" />
+              <h2 className="font-headline text-center">Stats</h2>
+              <Separator className="flex-1 bg-foreground" />
+            </div>
+            <div className="flex gap-10 items-center justify-center text-center">
+              <div className="flex flex-col space-y-1">
+                <h3 className="font-main font-bold text-sm">Highest Lvl.</h3>
+                <p>52</p>
+              </div>
+              <div className="flex flex-col space-y-1">
+                <h3 className="font-main font-bold text-sm">Score</h3>
+                <p>5505</p>
+              </div>
+              <div className="flex flex-col space-y-1">
+                <h3 className="font-main font-bold text-sm">Achievements</h3>
+                <p>{achievements.length}</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Achievements Section */}
+          <div className="flex flex-col space-y-8">
+            <div className="flex items-center gap-3">
+              <Separator className="flex-1 bg-foreground" />
+              <h2 className="font-headline text-center">Achievements</h2>
+              <Separator className="flex-1 bg-foreground" />
+            </div>
+
+            {/* Badges wall */}
+            <div className="grid grid-cols-3 gap-6 place-items-center">
+              {achievements.map((achievement) => (
+                <div
+                  key={achievement.id}
+                  className="flex flex-col items-center text-center space-y-2"
+                >
+                  <Image
+                    src={achievement.badgeURL}
+                    alt={achievement.title}
+                    width={80}
+                    height={80}
+                    className="rounded-full border border-gray-300 hover:border-black shadow-sm"
+                  />
+                  <p className="text-sm font-main">{achievement.title}</p>
                 </div>
-                <div>
-                  <IoIosArrowRoundForward size={24} />
-                </div>
-              </a>
-            ))}
-          </nav>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
