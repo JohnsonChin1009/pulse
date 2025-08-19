f745ba66b3a8ec928576d4c70ba40bda0a975395
Sjiaseng <TP065158@mail.apu.edu.my>
2025-08-19 15:37:52 +0800

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import LandingPage from "@/components/landing/landing";
 
-export default function LandingPage() {
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+
+export default function HomePage() {
   return (
-    <main className="py-10 flex flex-col space-y-6 px-4">
-      <Button variant="default">Login</Button>
-      <Button variant="destructive">Login</Button>
-      <Button variant="text">Login</Button>
-      <Button variant="icon">Login</Button>
-      <Button variant="outline">Login</Button>
-      <Button variant="secondary">Login</Button>
-      <Button variant="ghost">Login</Button>
-      <Button variant="link">Login</Button>
-      <Input placeholder="Email" />
-    </main>
+    <LandingPage />
+    // <main className="py-10 flex flex-col space-y-6 px-4">
+    //   <Button variant="default">Login</Button>
+    //   <Button variant="destructive">Login</Button>
+    //   <Button variant="text">Login</Button>
+    //   <Button variant="link">Login</Button>
+    //   <Button variant="outline">Login</Button>
+    //   <Button variant="secondary">Login</Button>
+    //   <Button variant="ghost">Login</Button>
+    //   <Button variant="link">Login</Button>
+    //   <Input placeholder="Email" />
+    // </main>
   );
 }
