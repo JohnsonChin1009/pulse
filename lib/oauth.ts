6bf3cafdf2ae426be5176d0d5ab781151e7cab4d
Johnson Chin <johnsonchin1009@gmail.com>
2025-08-20 15:25:20 +0800

    update: changed httpOnly true to false

--- lib/oauth.ts
+++ lib/oauth.ts
@@ -11,7 +11,7 @@ export async function sha256base64url(input: string) {
 
 export function cookieOpts() {
   return {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax" as const,
     path: "/",
