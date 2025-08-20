acb6c2d222f692f6033e9584b0cb0610c41004b2
Johnson Chin <johnsonchin1009@gmail.com>
2025-08-20 16:59:01 +0800

    fix: jia seng's cookies are missing

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
 
+export default function Dashboard() {
+  const [token, setToken] = useState("");
 
-export default function dashboard(){
-    return (
-        <div>
-            <p> This is admin dashboard, user and health practitioner go away hehehe </p>
-        </div>
-    )
+  useEffect(() => {
+    const fetchToken = async () => {
+      const res = await fetch(`/api/get-cookie`);
+      if (res.ok) {
+        const data = await res.json();
+        setToken(data.token);
+      } else {
+        setToken("No token found");
+      }
+    };
+    fetchToken();
+  }, []);
+
+  return (
+    <div>
+      <p>Token: {token}</p>
+    </div>
+  );
 }
+
